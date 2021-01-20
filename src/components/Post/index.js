1651c160f34dc58c2b5c61052b4cd06ff8e2dcd4
DavidSurina <david.surina22@gmail.com>
2021-01-20 11:33:49 +0100

    adressed some styling issues and changed bits of html

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -8,8 +8,8 @@ export default function Post({post}) {
         <div className='post-image-wrapper'>
             <img className='post-image' src={post.fields.image} />
             <div className='overlay'>
-              <p className='post-image-desc'><strong>Title:</strong> {post.fields.title}</p>
-              <p className='post-image-desc'><strong>User:</strong> {post.fields.userref.sys.id}</p>
+              <p className='post-image-desc-title'>{post.fields.title}</p>
+              <p className='post-image-desc'>By: {post.fields.userref.sys.id}</p>
             </div>
         </div>
     );
