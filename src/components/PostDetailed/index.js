13152f45cd5e51ea80a6389f29dd2b5c54244d41
vibueno <20514260+vibueno@users.noreply.github.com>
2021-01-22 01:58:45 +0100

    Format psot date

--- src/components/PostDetailed/index.js
+++ src/components/PostDetailed/index.js
@@ -8,14 +8,18 @@ function PostDetailed({ posts }) {
   const { postId } = useParams();
   const [postDetails, setPostDetails] = useState('');
 
-  useEffect(() => {
-    const getPostInfo = id => {
-      const post = posts.filter(post => post.sys.id === id)[0].fields;
-      return post;
-    };
+  const getPostInfo = id => {
+    const post = posts.filter(post => post.sys.id === id)[0].fields;
+    return post;
+  };
 
+  useEffect(() => {
     setPostDetails(getPostInfo(postId));
   }, [posts, postId]);
+
+  const formatDate = strDate => {
+    return strDate.substring(0, 10);
+  };
 
   return postDetails ? (
     <article className="post-entry">
@@ -30,7 +34,7 @@ function PostDetailed({ posts }) {
       <div className="post-entry__right-wrapper">
         <GoHomeButton className="post-entry__home-btn" caption="back" />
         <div className="post-entry__content">
-          <div className="post-entry__date">{postDetails.date}</div>
+          <div className="post-entry__date">{formatDate(postDetails.date)}</div>
           <div className="post-entry__description">
             {postDetails.description}
           </div>
