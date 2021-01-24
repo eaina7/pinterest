72acbae0387656cd5c84a36295d737e811a99cb0
vibueno <20514260+vibueno@users.noreply.github.com>
2021-01-24 02:44:57 +0100

    Component filter functions params

--- src/views/AllPosts/index.js
+++ src/views/AllPosts/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import PostList from '../../components/Posts/PostList';
 
 export default function AllPosts({ posts, users }) {
-  return <PostList posts={posts} users={users} />;
+  return <PostList posts={posts} />;
 }
--- src/views/BestPosts/index.js
+++ src/views/BestPosts/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PostList from '../../components/Posts/PostList';
 
 export default function BestPosts({ posts }) {
-  const getBestPosts = () => {
+  const getBestPosts = posts => {
     return posts.filter(post => post.rating === 5);
   };
 
-  return <PostList posts={getBestPosts()} />;
+  return <PostList posts={getBestPosts(posts)} />;
 }
